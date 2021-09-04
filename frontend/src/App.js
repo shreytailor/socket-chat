59ccef245e18f218b843050c33ab7eb9e89f8a97
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2021-09-04 19:37:13 +1200

    [Feature] Input modal for getting the user's name.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import 'App';
+import 'App.css';
+import Modal from 'components/General/InputModal';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+	const [name, setName] = useState("");
+
+	function onModalSubmit(name) {
+		setName(name);
+	}
+
+	return (
+		<div className="App">
+			<Modal 
+			title="Welcome To The Chat"  
+			description="Enter your name below to join the chat, and talk to others." 
+			buttonLabel="Join Server"
+			onSubmit={onModalSubmit}/>
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/General/InputModal.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styles from "components/General/InputModal.module.css";
+
+function InputModal(props) {
+    const [name, setName] = useState("");
+
+    function inputChangeHandler(event) {
+        setName(event.target.value);
+    }
+
+    function formSubmitHandler(event) {
+        event.preventDefault();
+        if (name.length === 0) {
+            return;
+        }
+
+        console.log(props.onSubmit);
+        props.onSubmit(name);
+    }
+
+    return (
+        <div className={styles.modal}>
+            <div className={styles.content}>
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+                <form onSubmit={formSubmitHandler}>
+                    <input type="text" value={name} onChange={inputChangeHandler}/>
+                    <button>{props.buttonLabel}</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default InputModal;
